badac17c7591c7d6b0d13389869db3cbd116534f
Waleed-HM <waleed.alhammami@spaceconcordia.ca>
2020-03-12 14:00:27 -0400

    [#334] Add functions on the wheel teensy setup code to request the needed data

--- robot/rover/new_IMU_code/new_IMU_code.ino
+++ robot/rover/new_IMU_code/new_IMU_code.ino
@@ -5,11 +5,35 @@
 #define LED 13
 #define imu_serial Serial2
 #define dev_serial Serial
-elapsedMillis timeElapsed;
 
+
+// Possible commands to send to the IMU to control the data 
+#define imu_cmd_switch      " "   // to stop/resume data transmission 
+#define imu_cmd_time        "t"   // to enable disable time stamps (not really useful)
+#define imu_cmd_acc         "a"   // accelerometer data 
+#define imu_cmd_gyro        "g"   // gyroscope data 
+#define imu_cmd_mag         "m"   // magnetometer data 
+#define imu_cmd_calc_raw    "c"   // to switch between raw/calculated readings
+#define imu_cmd_quat        "q"   // quaternion readings
+#define imu_cmd_euler       "e"   // Euler angles (pitch,roll,yaw)
+#define imu_cmd_heading     "h"   // heading data 
+#define imu_cmd_rate        "r"   // adjust rate of data transmission
+#define imu_cmd_acc_range   "A"   // adjust accelerometer full-scale imu_cmd_acc_range
+#define imu_cmd__gyro_range "G"   // adjust gyroscope full-scale range
+#define imu_cmd_sd          "s"   // to switch SD card data logging
+
+
+
+elapsedMillis timeElapsed;
 String imu_msg;
 long time_interval = 500;
 char cmd;
+
+
+void imu_send_command(char);
+
+
+
 void setup() {
   // put your setup code here, to run once:
   pinMode(LED,OUTPUT);
@@ -18,7 +42,10 @@ void setup() {
   dev_serial.begin(115200);
   // dev_serial.setTimeout(20);
   delay(300);
-  imu_serial.write(" ");
+  imu_send_command(imu_cmd_switch);
+  imu_send_command(imu_cmd_calc_raw);
+  imu_send_command(imu_cmd_euler);
+  imu_send_command(imu_cmd_heading);
   }
 
 void loop() {
@@ -33,7 +60,7 @@ void loop() {
   if (dev_serial.available() > 0)
   {
     cmd = dev_serial.read();
-    imu_serial.write(cmd);
+    imu_send_command(cmd);
   }
 
   if(imu_serial.available() > 0)
@@ -42,7 +69,13 @@ void loop() {
     imu_msg = imu_serial.readStringUntil('\n');
     dev_serial.println(imu_msg);
   }  
-  
+
+}
 
 
+
+
+void imu_send_command(char cmd)
+{
+  imu_serial.write(cmd);
 }
